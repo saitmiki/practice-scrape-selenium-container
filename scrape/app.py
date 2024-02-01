e8b1b7ba179a4355eafb04653e91219d91df04fd
saitmiki <doradora51018@gmail.com>
2024-02-01 13:49:37 +0900

    Initial commit

---
+++ scrape/app.py
@@ -0,0 +1,29 @@
+import json
+from selenium import webdriver
+
+
+def lambda_handler(event, context):
+    options = webdriver.ChromeOptions()
+    options.binary_location = '/opt/chrome/chrome'
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--single-process")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-dev-tools")
+    driver = webdriver.Chrome(
+        options=options, executable_path="/opt/chromedriver")
+    driver.get("https://example.com")
+    print(driver.title)
+    print(driver.page_source)
+    driver.close()
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps("done.")
+    }
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps("done.")
+    }
